53f07d3a2e83cdb2aeabedc8fa6486b08bbff446
caioeax <caio.ea@aluno.ifsc.edu.br>
2024-12-20 17:39:41 -0300

    feat: background Image

--- core/src/main/java/com/Josu/game/Josu.java
+++ core/src/main/java/com/Josu/game/Josu.java
@@ -14,15 +14,22 @@ public class Josu extends ApplicationAdapter {
 
     @Override
     public void create() {
+        Gdx.graphics.setWindowedMode(1920, 1080); // settei para começar em tela cheia já, não sei porq nao está aparecendo a barra de cima ainda
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("backgroundOsu.png");
     }
 
     @Override
-    public void render() {
+    public void render() { // calcular a escala proporcional, mantendo o aspecto da imagem, "codigo de baixo"
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        
+        float scale = Math.min(Gdx.graphics.getWidth() / (float) image.getWidth(),
+                               Gdx.graphics.getHeight() / (float) image.getHeight());
+        float x = (Gdx.graphics.getWidth() - image.getWidth() * scale) / 2;
+        float y = (Gdx.graphics.getHeight() - image.getHeight() * scale) / 2;
+    
+        batch.draw(image, x, y, image.getWidth() * scale, image.getHeight() * scale);
         batch.end();
     }
 
