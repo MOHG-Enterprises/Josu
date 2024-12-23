ba4af72408a3048419a497be8602babd698ee039
caioeax <caio.ea@aluno.ifsc.edu.br>
2024-12-23 17:45:15 -0300

    fix: ApplicationAdapter to Screen

--- core/src/main/java/com/Josu/game/JosuCatch.java
+++ core/src/main/java/com/Josu/game/JosuCatch.java
@@ -1,12 +1,10 @@
 package com.Josu.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -16,26 +14,30 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.Screen;
 
+public class JosuCatch implements Screen {
+    private final Josu game; // Referência ao jogo principal
+    private SpriteBatch spriteBatch;
+    private FitViewport viewport;
+    private Texture backgroundTexture;
+    private Texture fruitCatcherTexture;
+    private Texture dropTexture;
+    private Sound dropSound;
+    private Music music;
+    private Sprite fruitCatcherSprite;
+    private Array<Sprite> dropSprites;
+    private Vector2 touchPos;
+    private Rectangle fruitCatcherRectangle;
+    private Rectangle dropRectangle;
+    private float dropTimer;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class JosuCatch extends ApplicationAdapter {
-    SpriteBatch spriteBatch;
-    FitViewport viewport;
-    Texture backgroundTexture;
-    Texture fruitCatcherTexture;
-    Texture dropTexture;
-    Sound dropSound;
-    Music music;
-    Sprite fruitCatcherSprite;
-    Vector2 touchPos;
-    Array<Sprite> dropSprites;
-    float dropTimer;
-    Rectangle fruitCatcherRectangle;
-    Rectangle dropRectangle;
-    
+    public JosuCatch(Josu game) {
+        this.game = game;
+    }
+
     @Override
-    public void create() {
+    public void show() {
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(8, 5);
         backgroundTexture = new Texture("background.png");
@@ -43,71 +45,78 @@ public class JosuCatch extends ApplicationAdapter {
         dropTexture = new Texture("drop.png");
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.5f);
+        music.play();
+
         fruitCatcherSprite = new Sprite(fruitCatcherTexture);
         fruitCatcherSprite.setSize(1, 1);
         touchPos = new Vector2();
         dropSprites = new Array<>();
         fruitCatcherRectangle = new Rectangle();
         dropRectangle = new Rectangle();
-        music.setLooping(true);
-        music.setVolume(.5f);
-        music.play();
     }
 
     @Override
-    public void render() {
+    public void render(float delta) {
         input();
-        logic();
+        logic(delta);
         draw();
+
+        // Voltar ao menu principal
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(new MenuScreen(game));
+        }
     }
-    
+
     private void input() {
         float speed = 4f;
         float delta = Gdx.graphics.getDeltaTime();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             fruitCatcherSprite.translateX(speed * delta);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             fruitCatcherSprite.translateX(-speed * delta);
         }
         if (Gdx.input.isTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY()); // Get where the touch happened on screen
-            viewport.unproject(touchPos); // Convert the units to the world units of the viewport
-            fruitCatcherSprite.setCenterX(touchPos.x); // Change the horizontally centered position of the fruitCatcher
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(touchPos);
+            fruitCatcherSprite.setCenterX(touchPos.x);
         }
     }
-    
-    private void logic() {
+
+    private void logic(float delta) {
         float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
         float fruitCatcherWidth = fruitCatcherSprite.getWidth();
-        float fruitCatcherHeight = fruitCatcherSprite.getHeight();
-    
+
+        // Manter o sprite do catcher dentro da tela
         fruitCatcherSprite.setX(MathUtils.clamp(fruitCatcherSprite.getX(), 0, worldWidth - fruitCatcherWidth));
-    
-        float delta = Gdx.graphics.getDeltaTime();
-        // Apply the fruitCatcher position and size to the fruitCatcherRectangle
-        fruitCatcherRectangle.set(fruitCatcherSprite.getX(), fruitCatcherSprite.getY(), fruitCatcherWidth, fruitCatcherHeight);
-    
+
+        // Atualizar a posição e detectar colisões
+        fruitCatcherRectangle.set(
+            fruitCatcherSprite.getX(),
+            fruitCatcherSprite.getY(),
+            fruitCatcherSprite.getWidth(),
+            fruitCatcherSprite.getHeight()
+        );
+
         for (int i = dropSprites.size - 1; i >= 0; i--) {
             Sprite dropSprite = dropSprites.get(i);
-            float dropWidth = dropSprite.getWidth();
-            float dropHeight = dropSprite.getHeight();
-    
             dropSprite.translateY(-2f * delta);
-            // Apply the drop position and size to the dropRectangle
-            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
-    
-            if (dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
-            else if (fruitCatcherRectangle.overlaps(dropRectangle)) { // Check if the fruitCatcher overlaps the drop
-                dropSprites.removeIndex(i); // Remove the drop
+
+            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropSprite.getWidth(), dropSprite.getHeight());
+
+            if (dropSprite.getY() < -dropSprite.getHeight()) {
+                dropSprites.removeIndex(i);
+            } else if (fruitCatcherRectangle.overlaps(dropRectangle)) {
+                dropSprites.removeIndex(i);
                 dropSound.play();
             }
         }
-    
+
         dropTimer += delta;
-        if (dropTimer > .22f) {
+        if (dropTimer > 0.22f) {
             dropTimer = 0;
             createDroplet();
         }
@@ -119,44 +128,50 @@ public class JosuCatch extends ApplicationAdapter {
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
 
         spriteBatch.begin();
-        //{
-        
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-
-        spriteBatch.draw(fruitCatcherTexture, 0, 0, 1, 1); // draw the fruitCatcher
-        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight); // draw the background
+        spriteBatch.draw(backgroundTexture, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
         fruitCatcherSprite.draw(spriteBatch);
 
         for (Sprite dropSprite : dropSprites) {
             dropSprite.draw(spriteBatch);
         }
-
-         //}
         spriteBatch.end();
     }
 
     private void createDroplet() {
-        // create local variables for convenience
         float dropWidth = 1;
         float dropHeight = 1;
         float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-        
-        // create the drop sprite
+
         Sprite dropSprite = new Sprite(dropTexture);
         dropSprite.setSize(dropWidth, dropHeight);
         dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
-        dropSprite.setY(worldHeight);
-        dropSprites.add(dropSprite); // Add it to the list
+        dropSprite.setY(viewport.getWorldHeight());
+        dropSprites.add(dropSprite);
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height, true); // true centers the camera
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {
+        dispose();
     }
 
     @Override
     public void dispose() {
+        spriteBatch.dispose();
+        backgroundTexture.dispose();
+        fruitCatcherTexture.dispose();
+        dropTexture.dispose();
+        dropSound.dispose();
+        music.dispose();
     }
 }
